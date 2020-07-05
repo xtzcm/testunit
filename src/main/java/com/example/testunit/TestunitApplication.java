45eb45bd1501ebb6affba6e779db58884becf528
xtzcm <llh99992402616866@163.com>
2020-07-05 17:26:00 +0800

    init

---
+++ src/main/java/com/example/testunit/TestunitApplication.java
@@ -0,0 +1,12 @@
+package com.example.testunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestunitApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/testunit/TestunitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.testunit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestunitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
