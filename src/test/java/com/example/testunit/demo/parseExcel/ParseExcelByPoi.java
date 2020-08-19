73f0501c033cac7dabe50b0aa24d4a0d0966721f
xtzcm <llh99992402616866@163.com>
2020-08-19 21:17:00 +0800

    add poi

---
+++ src/test/java/com/example/testunit/demo/parseExcel/ParseExcelByPoi.java
@@ -0,0 +1,49 @@
+package com.example.testunit.demo.parseExcel;
+
+import org.apache.poi.POIXMLException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * @Despriction:
+ * @Author: wangcheng
+ * @Date: 2018/4/9 20:19
+ */
+public class ParseExcelByPoi {
+
+    @Test
+    public void testParseExcel() {
+        BufferedInputStream bis = null;
+        try {
+            bis = new BufferedInputStream(new FileInputStream("G:\\test\\test.xlsx"));
+            Workbook workbook = WorkbookFactory.create(bis);
+            System.out.println("sheet数量 = " + workbook.getNumberOfSheets());
+            if (workbook.getNumberOfSheets() > 0) {
+                for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+                    System.out.println("sheet" + (i + 1) + "有效行数 = " + workbook.getSheetAt(i).getLastRowNum());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (null != bis)
+                bis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
