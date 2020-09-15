fd5edebe1f2b8626c7b320731860b65f7621344d
xtzcm <llh99992402616866@163.com>
2020-09-15 22:19:02 +0800

    update

---
+++ src/test/java/com/example/testunit/demo/TestString.java
@@ -0,0 +1,9 @@
+package com.example.testunit.demo;
+
+/**
+ * @Despriction:
+ * @Author: wangcheng
+ * @Date: 2018/5/6 21:48
+ */
+public class TestString {
+}
---
+++ src/test/java/com/example/testunit/demo/collection/TestArray.java
@@ -0,0 +1,9 @@
+package com.example.testunit.demo.collection;
+
+/**
+ * @Despriction:
+ * @Author: wangcheng
+ * @Date: 2018/5/6 11:07
+ */
+public class TestArray {
+}
