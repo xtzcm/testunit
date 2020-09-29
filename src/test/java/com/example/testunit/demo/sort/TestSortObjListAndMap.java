9a939d04d09d1d0241ba66cea7124168d4bf93e7
xtzcm <llh99992402616866@163.com>
2020-09-29 16:05:45 +0800

    update

--- src/test/java/com/example/testunit/demo/sort/TestSortObjListAndMap.java
+++ src/test/java/com/example/testunit/demo/sort/TestSortObjListAndMap.java
@@ -12,7 +12,7 @@ import java.util.*;
  * @Author: wangcheng
  * @Date: 2018/5/3 21:36
  */
-public class TestSortObjList {
+public class TestSortObjListAndMap {
 
     public static List<ObjForSort> objList;
 
@@ -37,7 +37,7 @@ public class TestSortObjList {
      * 根据int字段进行排序
      */
     @Test
-    public void testSortByInt() {
+    public void testSortListByInt() {
         //常规方式
 //        Collections.sort(objList, new Comparator<ObjForSort>() {
 //            @Override
@@ -53,7 +53,7 @@ public class TestSortObjList {
      * 根据String字段排序
      */
     @Test
-    public void testSortByString(){
+    public void testSortListByString(){
         //常规方式
 //        Collections.sort(objList, new Comparator<ObjForSort>() {
 //            @Override
@@ -67,8 +67,41 @@ public class TestSortObjList {
             Collator collator = Collator.getInstance(Locale.CHINA);
             return collator.compare(o1.getName(), o2.getName());
         });
+    }
 
-
+    /**
+     * 根据ObjForSort对象的name进行排序
+     */
+    @Test
+    public void testSortMapByObjForSortName(){
+        //常规方式
+//        Map<ObjForSort, String> objMap = new TreeMap<>(new Comparator<ObjForSort>() {
+//            @Override
+//            public int compare(ObjForSort o1, ObjForSort o2) {
+//                int result = o1.getName().compareTo(o2.getName());
+//                if (0 == result)
+//                    return new Integer(o1.getAge()).compareTo(new Integer(o2.getAge()));
+//                return result;
+//            }
+//        });
+        //lambda表达式方式
+        Map<ObjForSort, String> objMap = new TreeMap<>((o1, o2) -> {
+            int result = o1.getName().compareTo(o2.getName());
+            if (0 == result)
+                return new Integer(o1.getAge()).compareTo(new Integer(o2.getAge()));
+            return result;
+        });
+        objMap.put(new ObjForSort("zhaoliu", 16), "深圳");
+        objMap.put(new ObjForSort("赵六", 16), "深圳");
+        objMap.put(new ObjForSort("王五", 15), "广州");
+        objMap.put(new ObjForSort("wangwu", 15), "广州");
+        objMap.put(new ObjForSort("李四", 14), "上海");
+        objMap.put(new ObjForSort("lisi", 14), "上海");
+        objMap.put(new ObjForSort("张三", 13), "北京");
+        objMap.put(new ObjForSort("zhangsan", 13), "北京");
+        for (Map.Entry<ObjForSort, String> entry : objMap.entrySet()) {
+            System.out.println(entry.getKey().getName() + " : " + entry.getValue());
+        }
     }
 }
 
